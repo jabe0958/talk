d4b4e9169f1017e66186281566478a1b82461ebe
jabe <jabe@ik1-332-26482.vs.sakura.ne.jp>
2018-04-30 22:25:38 +0900

    inmplement a PptpResponder

--- talk/models/pptp_responder.py
+++ talk/models/pptp_responder.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env
 
 from .abstract_responder import AbstractResponder
-import random
+import os, random
 
+PPTP_FILE = "../../resources/pptp.txt"
 
 class PptpResponder(AbstractResponder):
     def talk(self):
-        return "pptp"
+        messages = []
+        base_dir = os.path.dirname(__file__)
+        with open(os.path.join(base_dir, PPTP_FILE), "r") as f:
+            message = f.readline()
+            while message:
+                message = message.strip()
+                if len(message) > 0:
+                    messages.append(message)
+                message = f.readline()
+
+        if len(messages) == 0:
+            return "pptp error."
+
+        idx = random.randint(0, len(messages) - 1)
+        return messages[idx]
 
 if __name__ == "__main__":
     pptp = PptpResponder() 
